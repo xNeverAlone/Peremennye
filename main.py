4c98090d07d67cdb86f04d8c121bd8207ab01149
xNeverAlone <barretsv@gmail.com>
2024-11-21 20:11:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+kolich_vipolnen_dz = 12
+kolich_zatrat_chasov = 1.5
+name_of_course = 'Python'
+time_on_one_task = (kolich_zatrat_chasov/kolich_vipolnen_dz)
+print(name_of_course,',' "vsego zadach:", kolich_vipolnen_dz,',''zatracheno chasov:',kolich_zatrat_chasov,',' 'srednee vremya vipolneniya', time_on_one_task)
